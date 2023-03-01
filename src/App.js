eb4d101f75be378cff00bde1f2d6c2c96a623d0d
olcay-elt <olcay.elt@gmail.com>
2023-03-01 10:59:22 +0100

    Add new style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react'
 import "./App.css"
 import Home from './pages/Home'
 const App = () => {
-  return (
-    <div className="container">
-    <Home/>
-    </div>
-  )
+    return (
+        <div className="container">
+            <Home />
+        </div>
+    )
 }
 
 export default App
