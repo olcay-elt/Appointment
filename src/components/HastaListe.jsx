940cfe53cb2ca35faf8918de4785f025b6fa09f6
olcay-elt <olcay.elt@gmail.com>
2023-03-03 21:19:28 +0100

    Add

--- src/components/HastaListe.jsx
+++ src/components/HastaListe.jsx
@@ -1,37 +1,73 @@
 import React from "react";
 import { FaTimesCircle } from "react-icons/fa";
-const HastaListe = ({ hastalar, setHastalar }) => {
+const HastaListe = ({ hastalar, setHastalar, doktorlar }) => {
+
+
     return (
         <div>
             {hastalar.map((hasta) => {
 
                 return (
-                    <div>
-                        <div
-                            className={hasta.bittiMi ? "trueBittiStil" : "falseBitmediStil"}
-                            key={hasta.id}
-                            onDoubleClick={() =>
-                                setHastalar(
-                                    hastalar.map((i) =>
-                                        i.id === hasta.id ? { ...i, bittiMi: !i.bittiMi } : i
+                    <div key={hasta.id}>
+                        {doktorlar.length > 1 ? (
+                            <div
+                                className={hasta.bittiMi ? "trueBittiStil" : "falseBitmediStil"}
+                                key={hasta.id}
+                                onDoubleClick={() =>
+                                    setHastalar(
+                                        hastalar.map((i) =>
+                                            i.id === hasta.id ? { ...i, bittiMi: !i.bittiMi } : i
+                                        )
                                     )
-                                )
-                            }
-                        >
-                            <div>
-                                <h2>{hasta.text} </h2>
-                                <h4>{hasta.day} </h4>
-                                <h3>{hasta.doktorum} </h3>
-                            </div>
-
-                            <FaTimesCircle
-                                style={{ color: "red" }}
-                                onClick={() =>
-                                    setHastalar(hastalar.filter((insan) => insan.id !== hasta.id))
                                 }
-                            />
+                            >
+                                <div>
+                                    <h2>{hasta.text} </h2>
+                                    <h4>{hasta.day} </h4>
+                                    <h3>{hasta.doktorum} </h3>
+                                </div>
 
-                        </div>
+                                <FaTimesCircle
+                                    style={{ color: "red" }}
+                                    onClick={() =>
+                                        setHastalar(
+                                            hastalar.filter((insan) => insan.id !== hasta.id)
+                                        )
+                                    }
+                                />
+                            </div>
+                        ) :
+
+                            (doktorlar[0].doktor === hasta.doktorum &&
+
+                                (
+                                    <div
+                                        className={hasta.bittiMi ? "trueBittiStil" : "falseBitmediStil"}
+                                        key={hasta.id}
+                                        onDoubleClick={() =>
+                                            setHastalar(
+                                                hastalar.map((i) =>
+                                                    i.id === hasta.id ? { ...i, bittiMi: !i.bittiMi } : i
+                                                )
+                                            )
+                                        }
+                                    >
+                                        <div>
+                                            <h2>{hasta.text} </h2>
+                                            <h4>{hasta.day} </h4>
+                                            <h3>{hasta.doktorum} </h3>
+                                        </div>
+
+                                        <FaTimesCircle
+                                            style={{ color: "red" }}
+                                            onClick={() =>
+                                                setHastalar(
+                                                    hastalar.filter((insan) => insan.id !== hasta.id)
+                                                )
+                                            }
+                                        />
+                                    </div>
+                                ))}
                     </div>
                 );
             })}
